1430e205b16498219439735a4aa4d4f8398c3fcb
sidharthsaji04-sys <sidharthsaji04@gmail.com>
2026-08-27 10:18:13 +0530

    added the savings page

--- mainn.py
+++ mainn.py
@@ -5,10 +5,11 @@ import pandas as pd
 
 connect=sqlite3.connect("finance_tracker.db")
 cur=connect.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS Income(Date text DEFAULT CURRENT_DATE,Amount integer,Source text,Aim text)")
+cur.execute("CREATE TABLE IF NOT EXISTS Income(Date text DEFAULT CURRENT_DATE,Amount integer,Source text, Save_percent integer,Aim text)")
 cur.execute("CREATE TABLE IF NOT EXISTS Expense(Date text DEFAULT CURRENT_DATE,Amount integer,Merchant text,Description text)")
 cur.execute("CREATE TABLE IF NOT EXISTS Savings(Date text DEFAULT CURRENT_DATE,Amount integer,Source text,savings integer,Total integer)")
 connect.commit()
+
 
 class Finance:
     def validate_entry(self,amount, *text_fields):
@@ -25,6 +26,9 @@ class Finance:
             i_amount=st.number_input('Enter the amount in rupees: ')
             i_source=st.text_input('Enter the source of the money: ')
             i_aim=st.text_input('Enter your plan on how to spend this money: ')
+            i_save_percent=st.number_input('How much percentage do you like to save: ',max_value=100,min_value=0)
+            i_balance=i_amount - i_amount*(i_save_percent/100)
+            i_saving=i_amount*(i_save_percent/100)
             i_date=date.today()
             error=self.validate_entry(i_amount,i_source,i_aim)
             submit_button=st.form_submit_button(label='Enter')
@@ -32,9 +36,10 @@ class Finance:
                 if error:
                   st.error(error)
                 else:
-                  cur.execute("INSERT INTO Income(Date,Amount,Source,Aim) VALUES(?,?,?,?)",(i_date,i_amount,i_source,i_aim))
+                  cur.execute("INSERT INTO Income(Date,Amount,Source,Save_percent,Aim,Balance) VALUES(?,?,?,?,?,?)",(i_date,i_amount,i_source,i_save_percent,i_aim,i_balance))
+                  cur.execute("INSERT INTO Savings(Date,Amount,Source,savings) VALUES(?,?,?,?)",(i_date,i_amount,i_source,i_saving))
                   connect.commit()
-                  st.write('entry saved successfully')
+                  st.write(f'entry saved successfully, added {i_balance}rupees')
 
         if st.button('Back to home'):
             st.session_state.page='home'
@@ -43,8 +48,10 @@ class Finance:
         if st.button('Income history👇'):
             df=pd.read_sql_query('SELECT*FROM Income',connect)
             st.dataframe(df)
+            cur.execute("SELECT SUM(amount) FROM Income")
+            total_income = cur.fetchone()[0]
 
-
+            st.metric("Total Income", f"₹{total_income}")
 
     def e_entry(self):
         st.header('EXPENSE ENTRY PAGE')
@@ -64,12 +71,21 @@ class Finance:
                         st.write('Entry saved successfully')
 
         if st.button('Back to home'):
-            st.session_state.page='home'
-            st.rerun()
+             st.session_state.page='home'
+             st.rerun()
 
         if st.button('Expense history'):
             df=pd.read_sql_query('SELECT*FROM Expense',connect)
             st.dataframe(df)
+
+
+    def savings(self):
+        st.header('SAVINGS')
+        df=pd.read_sql_query('SELECT*FROM Savings',connect)
+        st.dataframe(df)
+        cur.execute("SELECT SUM(savings) FROM savings")
+        total_saved = cur.fetchone()[0]
+        st.metric("Total Savings", f"₹{total_saved}")
 
 
 def home_page():
@@ -103,10 +119,10 @@ elif st.session_state.page=='in':
     f.i_entry()
 elif st.session_state.page=='ex':
     f.e_entry()
+elif st.session_state.page=='sa':
+    f.savings()
 
- # add the savings page and add the feature to link the income with savings
- # create the math for the saving percent from the income
- # create a dashboard for each containing a graph and total           
+          
     
 
         
