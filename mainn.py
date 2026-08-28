bf2d0497aa47b3ba8335003972af207ba5af6adb
sidharthsaji04-sys <sidharthsaji04@gmail.com>
2026-08-28 11:25:32 +0530

    added some new features

--- mainn.py
+++ mainn.py
@@ -2,6 +2,7 @@ import sqlite3
 import streamlit as st
 from datetime import date 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 connect=sqlite3.connect("finance_tracker.db")
 cur=connect.cursor()
@@ -17,16 +18,19 @@ class Finance:
          return "Amount must be greater than 0."
         for field in text_fields:
             if field.strip() == "":
-              return "All fields must be filled in."
+              return "All fields must be filled in. Enter all values again"
         return None 
         
     def i_entry(self):
         st.header('INCOME ENTRY PAGE')
-        with st.form(key='sample'):
+        with st.form(key='sample',clear_on_submit=True):
             i_amount=st.number_input('Enter the amount in rupees: ')
-            i_source=st.text_input('Enter the source of the money: ')
-            i_aim=st.text_input('Enter your plan on how to spend this money: ')
-            i_save_percent=st.number_input('How much percentage do you like to save: ',max_value=100,min_value=0)
+            sources=['relatives','bus_fee_balance','random_find','others']
+            i_source=st.selectbox('Source',sources)
+            aims=['no idea','fun','book','others','travel']
+            i_aim=st.selectbox('Enter your plan on how to spend this money: ',aims)
+            percentages=[0,10,20,50,80,100]
+            i_save_percent=st.selectbox('Saving_percentage',percentages)
             i_balance=i_amount - i_amount*(i_save_percent/100)
             i_saving=i_amount*(i_save_percent/100)
             i_date=date.today()
@@ -50,14 +54,21 @@ class Finance:
             st.dataframe(df)
             cur.execute("SELECT SUM(amount) FROM Income")
             total_income = cur.fetchone()[0]
-
             st.metric("Total Income", f"₹{total_income}")
+        if st.button('Show graphically'):
+                df=pd.read_sql_query("SELECT Date,sum(Amount) AS Amount from Income GROUP BY Date ORDER BY Date",connect)
+                plt.plot(df['Date'],df['Amount'])
+                st.pyplot(plt)
+        st.image('dollar.jpg',use_container_width=True)
+                
+            
 
     def e_entry(self):
         st.header('EXPENSE ENTRY PAGE')
-        with st.form(key='sample'):
+        with st.form(key='sample',clear_on_submit=True):
                 e_amount=st.number_input('Enter the amount in rupees: ')
-                e_merchant=st.text_input('Enter where you spent money: ')
+                merchant=['food','travel','fun','clothes','books','others']
+                e_merchant=st.selectbox('Where you spent the money',merchant)
                 e_description=st.text_input('Add a description: ')
                 e_date=date.today()
                 error=self.validate_entry(e_amount,e_merchant,e_description)
@@ -76,7 +87,14 @@ class Finance:
 
         if st.button('Expense history'):
             df=pd.read_sql_query('SELECT*FROM Expense',connect)
-            st.dataframe(df)
+            st.dataframe(df) 
+
+        if st.button('Show graphically'):
+                df=pd.read_sql_query("SELECT Date,sum(Amount) AS Amount from Expense GROUP BY Date ORDER BY Date",connect)
+                plt.plot(df['Date'],df['Amount'])
+                st.pyplot(plt)
+
+        st.image('dollar.jpg',use_container_width=True)
 
 
     def savings(self):
@@ -86,12 +104,14 @@ class Finance:
         cur.execute("SELECT SUM(savings) FROM savings")
         total_saved = cur.fetchone()[0]
         st.metric("Total Savings", f"₹{total_saved}")
+        st.image('dollar.jpg',use_container_width=True)
 
 
 def home_page():
     st.header("Sidharth's personal Finance Tracker💸")
 
     col1,col2,col3=st.columns(3)
+    st.image('money.jpg',use_container_width=True)
 
     with col1:
         if st.button('Income',use_container_width=True):
@@ -123,7 +143,3 @@ elif st.session_state.page=='sa':
     f.savings()
 
           
-    
-
-        
-
